839151e328f17577f3693f71fdb86b8433624ba2
temple <970450647@qq.com>
2018-01-29 17:53:57 +0800

    第四章教程完毕

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
-// const ExtractPlugin = require('extract-text-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -9,7 +9,7 @@ const config = {
     target: "web",
     entry: path.join(__dirname, 'src/index.js'), 
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module:{
@@ -27,20 +27,6 @@ const config = {
                 use: [
                     'style-loader',
                     'css-loader'
-                ]
-            },
-            {
-                test: /\.styl/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap:true
-                        }
-                    },
-                    'stylus-loader'
                 ]
             },
             {
@@ -67,7 +53,21 @@ const config = {
     ]
 }
 
-if (isDev) {
+if (isDev) {  //开发环境
+    config.module.rules.push({
+        test: /\.styl/,
+        use:[
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap:true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port: 8000,
@@ -81,6 +81,37 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use:ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use:[
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap:true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        })
     )
 }
 module.exports = config
